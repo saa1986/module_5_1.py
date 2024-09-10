3382f5a6d20c66a934f543708bf5d390d777cfa5
saa1986 <1986zip@mail.ru>
2024-09-10 18:41:44 +0500

    Add files via upload

---
+++ module_5_1.py
@@ -0,0 +1,19 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+
+# Создание объектов класса House
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+
+# Вызов метода go_to
+h1.go_to(5)
+h2.go_to(10)
